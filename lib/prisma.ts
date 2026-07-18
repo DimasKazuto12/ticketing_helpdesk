d2ca3202580d04989c7249ccb4fa769caee2ba65
DimasKazuto12 <kirito22022008@gmail.com>
2026-07-18 11:15:59 +0700

    Mengatasi connection timeout database yang crash

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const prismaClientSingleton = () => {
    return new PrismaClient()
+    .$extends(withAccelerate())
 }
 
 declare global {
